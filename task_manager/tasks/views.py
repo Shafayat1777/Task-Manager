2223bdca3a3f9cd6d84470e74d39fbbc2b1e584a
Shafayat <shafayat1777@gmail.com>
2024-02-09 05:38:57 +0600

    added template

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
